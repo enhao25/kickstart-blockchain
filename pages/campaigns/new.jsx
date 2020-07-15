65da0829e152852a4f77d1ef87322e32fc9ae224
enhao25 <larloller@gmail.com>
2020-07-15 08:27:06 +0800

    Complete new campaign form submission

--- pages/campaigns/new.jsx
+++ pages/campaigns/new.jsx
@@ -1,17 +1,56 @@
 import React from 'react';
 import Layout from '../../components/Layout';
-import { Button, Form } from 'semantic-ui-react';
+import { Button, Form, Input, Message } from 'semantic-ui-react';
+
+import web3 from '../../ethereum/web3';
+import factory from "../../ethereum/factory";
 
 class CampaignNew extends React.Component {
+    state = {
+        mininumContribution: '',
+        errorMsg: '',
+        loading: false
+    };
+
+    inputChange = event => {
+        this.setState({ mininumContribution: event.target.value })
+    }
+
+    onSumbit = async event => {
+        event.preventDefault();
+        
+        this.setState({ loading: true, errorMsg: "" })
+
+        const accounts = await web3.eth.getAccounts();
+        try {
+            await factory.methods
+                .createCampaign(this.state.mininumContribution)
+                .send({
+                    from: accounts[0]
+                })
+        } catch (err) {
+            this.setState({ errorMsg: err.message })
+        }
+
+        this.setState({ loading: false })
+        
+    }
+
     render() {
         return (
             <Layout>
                 <h3>Create a Campaign</h3>
-                <Form>
+                <Form onSubmit={this.onSumbit} error={!!this.state.errorMsg}>
                     <Form.Field>
                         <label>Minimum Contribution</label>
-                        <input />
-                        <Button primary>Create!</Button>
+                        <Input 
+                            label='wei' 
+                            labelPosition='right'
+                            value={this.state.mininumContribution}
+                            onChange={this.inputChange}
+                        />
+                        <Message error header="Oops!" content={this.state.errorMsg} />
+                        <Button loading={this.state.loading} primary>Create!</Button>
                     </Form.Field>
                 </Form>
             </Layout>
