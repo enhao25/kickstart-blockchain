6af2768f16303d6878a95eef273f937a1097e5f6
enhao25 <larloller@gmail.com>
2020-07-19 09:26:28 +0800

    Ask the user to connect with metamask

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -3,18 +3,25 @@ import { Container } from 'semantic-ui-react';
 import Head from "next/head";
 import Navbar from './Navbar';
 
-const Layout = props => {
-    return(
-        <Container style={{ marginTop: '10px' }}>
-            <Head>
-                {/* Application header */}
-                <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
-            </Head>
-            <Navbar />
-            {props.children}
-            {/* <h1>Im a footer</h1> */}
-        </Container>
-    )
+class Layout extends React.Component{
+    componentDidMount() {
+        // Enable ethereum on the webapge
+        window.ethereum.enable();
+    }
+    
+    render() {
+        return(
+            <Container style={{ marginTop: '10px' }}>
+                <Head>
+                    {/* Application header */}
+                    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
+                </Head>
+                <Navbar />
+                {this.props.children}
+                {/* <h1>Im a footer</h1> */}
+            </Container>
+        )
+    }
 }
 
 export default Layout;
